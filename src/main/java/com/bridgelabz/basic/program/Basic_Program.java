2255a4bd6a4d4b38bc50b2c6854aa52ce76af383
sangharsh23 <sangharsh355@gmail.com>
2021-08-27 23:11:25 +0530

    Added: Welcome msg

---
+++ src/main/java/com/bridgelabz/basic/program/Basic_Program.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.basic.program;
+
+public class Basic_Program {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic Core Program");
+    }
+}
+
