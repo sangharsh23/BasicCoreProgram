1829d03a6fe59add91204912bc94cfd8272c8b60
sangharsh23 <sangharsh355@gmail.com>
2021-08-28 00:27:05 +0530

    Added: Swap two no Program

---
+++ src/main/java/com/bridgelabz/basic/program/Swap_Two_No.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.basic.program;
+
+public class Swap_Two_No {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 10;
+
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
+
+
+
+
