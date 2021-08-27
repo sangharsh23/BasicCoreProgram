4e2d8c275d1ea17c72bc135fa1f02335b65e681d
sangharsh23 <sangharsh355@gmail.com>
2021-08-27 23:27:06 +0530

    Added: coin filp

---
+++ src/main/java/com/bridgelabz/basic/program/Coin_Toss.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.basic.program;
+
+public class Coin_Toss {
+    public static void main(String[] args) {
+        if(Math.random() < 0.5) {
+            System.out.println("Head");
+        }
+        else{
+            System.out.println("Tail");
+        }
+    }
+}
