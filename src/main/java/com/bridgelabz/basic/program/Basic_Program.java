b515211616f63c6566ecff3a1c5e5410289e88e2
sangharsh23 <sangharsh355@gmail.com>
2021-08-28 14:24:34 +0530

    Added: program swap two number

--- src/main/java/com/bridgelabz/basic/program/Basic_Program.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.basic.program;
-
-public class Basic_Program {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Basic Core Program");
-    }
-}
-
