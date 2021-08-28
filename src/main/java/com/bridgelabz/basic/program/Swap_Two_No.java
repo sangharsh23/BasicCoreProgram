053818f584589083cc09bb49ecac773d3881b72a
sangharsh23 <sangharsh355@gmail.com>
2021-08-28 13:21:04 +0530

    Added: program to swap two num.

--- src/main/java/com/bridgelabz/basic/program/Swap_Two_No.java
+++ src/main/java/com/bridgelabz/basic/program/Swap_Two_No.java
@@ -1,16 +1,27 @@
 package com.bridgelabz.basic.program;
 
+import java.util.Scanner;
+
 public class Swap_Two_No {
     public static void main(String[] args) {
-        int x = 20;
-        int y = 10;
+
+        int num1 = 0;
+        int num2 = 0;
+
+        Scanner no = new Scanner(System.in);
+        System.out.println("Enter the num1 before swap: ");
+        num1 = no.nextInt();
+        System.out.println("Enter the num2 before swap");
+        num2 = no.nextInt();
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
 
 
-        x = x + y;
-        y = x - y;
-        x = x - y;
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
+        System.out.println("num1 and num2 after swapping ");
+        System.out.print("num1 = " + num1 + ", num2 = " + num2);
+
     }
 }
 
