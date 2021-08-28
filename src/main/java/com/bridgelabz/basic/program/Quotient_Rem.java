30f39dd6cbc96c33b5d2c6c19b619664d0a4bd67
sangharsh23 <sangharsh355@gmail.com>
2021-08-28 15:06:10 +0530

    Added: program Quotient and remainder

--- src/main/java/com/bridgelabz/basic/program/Quotient_Rem.java
+++ src/main/java/com/bridgelabz/basic/program/Quotient_Rem.java
@@ -1,15 +1,23 @@
 package com.bridgelabz.basic.program;
 
+import java.util.Scanner;
+
 public class Quotient_Rem {
         public static void main(String[] args) {
 
-            int dividend = 28;
-            int div_sor = 8;
+            float dividend = 0,div_sor = 0;
+            Scanner qo = new Scanner(System.in);
 
-            int quotient = dividend / div_sor;
+            System.out.println("Enter the divided value : ");
+            dividend = qo.nextInt();
+
+            System.out.println("Enter the divisor value : ");
+            div_sor = qo.nextInt();
+
+            float quotient = dividend / div_sor;
             System.out.println("Quotient = " + quotient);
 
-            int remainder = dividend - (div_sor * quotient);
+            float remainder = dividend % div_sor;
             System.out.println("Remainder = " + remainder);
         }
     }
